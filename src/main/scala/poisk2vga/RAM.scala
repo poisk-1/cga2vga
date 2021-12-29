51c432eefc76e626300787882a544f3a57a7767d
Peter Hizalev <peter.hizalev@gmail.com>
2021-12-28 20:05:55 -0800

    Frame buffer

---
+++ src/main/scala/poisk2vga/RAM.scala
@@ -0,0 +1,63 @@
+package poisk2vga
+
+import chisel3._
+import chisel3.util.HasBlackBoxInline
+import chisel3.util.log2Ceil
+
+class RAM(width: Int, depth: Int) extends BlackBox with HasBlackBoxInline {
+
+  override def desiredName: String = s"rams_tdp_rf_rf_${width}_${depth}"
+
+  val addressWidth = log2Ceil(depth)
+
+  val io = IO(new Bundle {
+    val clka = Input(Clock())
+    val wea = Input(Bool())
+    val ena = Input(Bool())
+    val addra = Input(UInt(addressWidth.W))
+    val dia = Input(UInt(width.W))
+    val doa = Output(UInt(width.W))
+
+    val clkb = Input(Clock())
+    val web = Input(Bool())
+    val enb = Input(Bool())
+    val addrb = Input(UInt(addressWidth.W))
+    val dib = Input(UInt(width.W))
+    val dob = Output(UInt(width.W))
+  })
+
+  setInline(
+    s"$desiredName.v",
+    s"""
+       |module $desiredName (clka,clkb,ena,enb,wea,web,addra,addrb,dia,dib,doa,dob);
+       |
+       |input clka,clkb,ena,enb,wea,web;
+       |input [${addressWidth - 1}:0] addra,addrb;
+       |input [${width - 1}:0] dia,dib;
+       |output [${width - 1}:0] doa,dob;
+       |reg [${width - 1}:0] ram [${depth - 1}:0];
+       |reg [${width - 1}:0] doa,dob;
+       |
+       |always @(posedge clka)
+       |begin
+       |  if (ena)
+       |    begin
+       |      if (wea)
+       |        ram[addra] <= dia;
+       |      doa <= ram[addra];
+       |    end
+       |end
+       |
+       |always @(posedge clkb)
+       |begin
+       |  if (enb)
+       |    begin
+       |      if (web)
+       |        ram[addrb] <= dib;
+       |      dob <= ram[addrb];
+       |    end
+       |end
+       |
+       |endmodule""".stripMargin
+  )
+}
--- src/main/scala/poisk2vga/Top.scala
+++ src/main/scala/poisk2vga/Top.scala
@@ -3,7 +3,7 @@ package poisk2vga
 import chisel3._
 import chisel3.util.log2Ceil
 
-class Top extends Module {
+class Top extends RawModule {
   val H_POLARITY = false
   val H_SYNC = 112
   val H_BPORCH = 144
@@ -23,52 +23,81 @@ class Top extends Module {
   val V_BPORCH_ADJ = V_BPORCH - 1
 
   val io = IO(new Bundle {
+    val reset = Input(Bool())
+    val capClock = Input(Clock())
+    val genClock = Input(Clock())
     val syncGpio = Input(UInt(4.W))
     val rgbiGpio = Input(UInt(4.W))
     val address = Output(UInt(log2Ceil(H_ACTIVE * V_ACTIVE).W))
-    val valid = Output(Bool())
     val rgbi = Output(UInt(4.W))
   })
 
-  val hSync = RegNext(RegNext(io.syncGpio(0), (!H_POLARITY).B), (!H_POLARITY).B)
-  val vSync = RegNext(RegNext(io.syncGpio(1), (!V_POLARITY).B), (!V_POLARITY).B)
-  val rgbi = RegNext(RegNext(RegNext(io.rgbiGpio, 0.U), 0.U), 0.U)
+  val frameBuffer = Module(new RAM(4, H_ACTIVE * V_ACTIVE))
 
-  val vAddress = Wire(UInt(log2Ceil(V_ACTIVE).W))
-  val hAddress = Wire(UInt(log2Ceil(H_ACTIVE).W))
+  frameBuffer.io.clka := io.capClock
+  frameBuffer.io.clkb := io.genClock
 
-  val vValid = Wire(Bool())
-  val hValid = Wire(Bool())
+  withClockAndReset(io.capClock, io.reset) {
+    val hSync =
+      RegNext(RegNext(io.syncGpio(0), (!H_POLARITY).B), (!H_POLARITY).B)
+    val vSync =
+      RegNext(RegNext(io.syncGpio(1), (!V_POLARITY).B), (!V_POLARITY).B)
+    val rgbi = RegNext(RegNext(RegNext(io.rgbiGpio, 0.U), 0.U), 0.U)
 
-  val hReceiver = Module(
-    new Receiver(H_SYNC, 1, H_BPORCH_ADJ + H_ACTIVE, H_POLARITY)
-  )
+    val vAddress = Wire(UInt(log2Ceil(V_ACTIVE).W))
+    val hAddress = Wire(UInt(log2Ceil(H_ACTIVE).W))
 
-  hReceiver.io.sync := hSync
+    val vValid = Wire(Bool())
+    val hValid = Wire(Bool())
 
-  hAddress := hReceiver.io.address - H_BPORCH_ADJ.U
-  hValid := hReceiver.io.valid && (hReceiver.io.address >= H_BPORCH_ADJ.U)
+    val hCapture = Module(
+      new Receiver(H_SYNC, 1, H_BPORCH_ADJ + H_ACTIVE, H_POLARITY)
+    )
 
-  withClock(hReceiver.io.valid.asClock) {
-    val vReceiver =
-      Module(
-        new Receiver(
-          V_SYNC,
-          1,
-          V_BPORCH_ADJ + V_ACTIVE,
-          V_POLARITY
+    hCapture.io.sync := hSync
+
+    hAddress := hCapture.io.address - H_BPORCH_ADJ.U
+    hValid := hCapture.io.valid && (hCapture.io.address >= H_BPORCH_ADJ.U)
+
+    withClock(hCapture.io.valid.asClock) {
+      val vCapture =
+        Module(
+          new Receiver(
+            V_SYNC,
+            1,
+            V_BPORCH_ADJ + V_ACTIVE,
+            V_POLARITY
+          )
         )
-      )
 
-    vReceiver.io.sync := vSync
+      vCapture.io.sync := vSync
 
-    vAddress := vReceiver.io.address - V_BPORCH_ADJ.U
-    vValid := vReceiver.io.valid && (vReceiver.io.address >= V_BPORCH_ADJ.U)
+      vAddress := vCapture.io.address - V_BPORCH_ADJ.U
+      vValid := vCapture.io.valid && (vCapture.io.address >= V_BPORCH_ADJ.U)
+    }
+
+    frameBuffer.io.ena := vValid && hValid
+    frameBuffer.io.wea := vValid && hValid
+    frameBuffer.io.addra := vAddress * H_ACTIVE.U + hAddress
+    frameBuffer.io.dia := rgbi
   }
 
-  io.address := vAddress * H_ACTIVE.U + hAddress
-  io.valid := vValid && hValid
-  io.rgbi := rgbi
+  withClockAndReset(io.genClock, io.reset) {
+    val genAddress = RegInit(UInt(log2Ceil(H_ACTIVE * V_ACTIVE).W), 0.U)
+
+    when(genAddress === (H_ACTIVE * V_ACTIVE - 1).U) {
+      genAddress := 0.U
+    }.otherwise {
+      genAddress := genAddress + 1.U
+    }
+
+    frameBuffer.io.enb := true.B
+    frameBuffer.io.web := false.B
+    frameBuffer.io.addrb := genAddress
+
+    io.rgbi := frameBuffer.io.dob
+    io.address := genAddress
+  }
 }
 
 object Top extends App {
